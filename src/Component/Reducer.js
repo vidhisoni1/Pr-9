5020ee592d5baeed8c55dd97dfbf922a3cd433cc
vidhisoni <vidhisoni8869#gmail.com>
2024-01-17 19:16:00 +0530

    push

---
+++ src/Component/Reducer.js
@@ -0,0 +1,19 @@
+// reducers.js
+const initialState = {
+    cartItems: [],
+  };
+  
+  const cartReducer = (state = initialState, action) => {
+    switch (action.type) {
+      case 'ADD_TO_CART':
+        return {
+          ...state,
+          cartItems: [...state.cartItems, action.payload],
+        };
+      default:
+        return state;
+    }
+  };
+  
+  export default cartReducer;
+  
